2fb29a1b277e42b724e8f12bca0e8a81294e7cf8
Diana <diudiana7788@gmail.com>
2025-07-28 10:19:43 -0700

    finsihed root UI and new to do UI

--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -10,12 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Text("To Do List")
+                    .font(.title)
+                    .fontWeight(.black)
+                Spacer()
+                Button{
+                    
+                }label: {
+                    Text("+")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+                
+                //this button syntax allows more modifiers on the "+"
+                
+            }
+            .padding()
+            Spacer()
         }
         .padding()
+        
     }
 }
 
